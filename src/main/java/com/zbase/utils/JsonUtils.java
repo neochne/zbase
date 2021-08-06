9933c2cd4b2131571ceeaa20c0f879a1735ab6de
Neo Chen <neochne@gmail.com>
2021-08-06 08:47:21 +0800

    Update JsonUtils.java
    opt

--- src/main/java/com/zbase/utils/JsonUtils.java
+++ src/main/java/com/zbase/utils/JsonUtils.java
@@ -14,96 +14,147 @@ public final class JsonUtils {
     }
 
     public static String getValue(JSONObject object, String key) {
-        if (object == null
-                || key == null
-                || "".equals(key.trim())) {
-            return "";
-        }
-        String value = null;
         try {
-            if (object.has(key)) {
-                value = String.valueOf(object.get(key));
-            }
+            return (object == null || key == null || "".equals(key.trim()) || !object.has(key) || object.isNull(key)) ? "" : String.valueOf(object.get(key));
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        return value == null || "null".equals(value) ? "" : value;
+        return "";
     }
 
-    public static String getValue(JSONArray array, int index) {
-        if (array == null || array.length() < 1 || index >= array.length()) {
-            return "";
-        }
-        String value = null;
+    public static String getValueTrust(JSONObject object, String key) {
         try {
-            value = array.getString(index);
+            return String.valueOf(object.get(key));
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        return value == null || "null".equals(value) ? "" : value;
+        return "";
+    }
+
+    public static int getIntValue(JSONObject object, String key) {
+        try {
+            return (object == null || key == null || "".equals(key.trim()) || !object.has(key)) ? 0 : object.getInt(key);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public static int getIntValueTrust(JSONObject object, String key) {
+        try {
+            return object.getInt(key);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
     }
 
     public static JSONObject getJSONObject(JSONObject object, String key) {
-        if (object == null || key == null || "".equals(key.trim())) {
-            return new JSONObject();
-        }
-        if (!object.has(key)) {
-            return new JSONObject();
-        }
-        JSONObject jsonObject = null;
         try {
-            jsonObject = object.getJSONObject(key);
+            return (object == null || key == null || "".equals(key.trim()) || !object.has(key) || object.isNull(key)) ? new JSONObject() : object.getJSONObject(key);
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        return jsonObject == null ? new JSONObject() : jsonObject;
+        return new JSONObject();
     }
 
-    public static JSONObject getJSONObject(JSONArray array, int index) {
-        if (array == null || array.length() < 1 || index >= array.length()) {
-            return new JSONObject();
-        }
-        JSONObject jsonObject = null;
+    public static JSONObject getJSONObjectTrust(JSONObject object, String key) {
         try {
-            jsonObject = array.getJSONObject(index);
+            return object.getJSONObject(key);
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        return jsonObject == null ? new JSONObject() : jsonObject;
+        return new JSONObject();
     }
 
     public static JSONArray getJSONArray(JSONObject object, String key) {
-        if (object == null || key == null || "".equals(key.trim())) {
-            return new JSONArray();
-        }
-        if (!object.has(key)) {
-            return new JSONArray();
-        }
-        JSONArray array = null;
         try {
-            array = object.getJSONArray(key);
+            return (object == null || key == null || "".equals(key.trim()) || !object.has(key) || object.isNull(key)) ? new JSONArray() : object.getJSONArray(key);
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        return array == null ? new JSONArray() : array;
-    }
-    
-    public static JSONArray constructArray(String arrayStr) {
-        try {
-            return new JSONArray(arrayStr);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        return null;
+        return new JSONArray();
     }
 
-    public static JSONObject constructObject(String arrayStr) {
+    public static JSONArray getJSONArrayTrust(JSONObject object, String key) {
         try {
-            return new JSONObject(arrayStr);
+            return object.getJSONArray(key);
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        return null;
+        return new JSONArray();
+    }
+
+    public static String getValue(JSONArray array, int index) {
+        try {
+            return (array == null || array.length() < 1 || index >= array.length() || array.isNull(index)) ? "" : String.valueOf(array.get(index));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    public static String getValueTrust(JSONArray array, int index) {
+        try {
+            return String.valueOf(array.get(index));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    public static int getIntValue(JSONArray array, int index) {
+        try {
+            return (array == null || array.length() < 1 || index >= array.length()) ? 0 : array.getInt(index);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public static int getIntValueTrust(JSONArray array, int index) {
+        try {
+            return array.getInt(index);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public static JSONObject getJSONObject(JSONArray array, int index) {
+        try {
+            return (array == null || array.length() < 1 || index >= array.length() || array.isNull(index)) ? new JSONObject() : array.getJSONObject(index);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return new JSONObject();
+    }
+
+    public static JSONObject getJSONObjectTrust(JSONArray array, int index) {
+        try {
+            return array.getJSONObject(index);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return new JSONObject();
+    }
+
+    public static JSONArray constructJSONArray(String jsonStr) {
+        try {
+            return new JSONArray(jsonStr);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return new JSONArray();
+    }
+
+    public static JSONObject constructJSONObject(String jsonStr) {
+        try {
+            return new JSONObject(jsonStr);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return new JSONObject();
     }
 
 }
