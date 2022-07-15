8010ac1f36029470bf377ed8ce3573633f66f8d8
sharp <sharpchencn@gmail.com>
2022-07-15 17:02:40 +0800

    update tool bar example activity

--- src/main/java/com/zbase/activity/ToolbarActivityExample.java
+++ src/main/java/com/zbase/activity/ToolbarActivityExample.java
@@ -3,28 +3,33 @@ package com.zbase.activity;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.zbase.R;
 import com.zbase.view.ToolbarLayout;
+import com.zbase.x.lp.FrameLayoutParamsX;
 
 public abstract class ToolbarActivityExample extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(createContentView(createToolbar()));
+        onCreateContentView(createToolbar());
     }
 
-    public abstract ToolbarLayout createContentView(ToolbarLayout toolbarLayout);
+    @Override
+    public void setContentView(View view) {
+        super.setContentView(view, new FrameLayoutParamsX(FrameLayoutParamsX.MATCH_PARENT, FrameLayoutParamsX.MATCH_PARENT));
+    }
+
+    public abstract void onCreateContentView(ToolbarLayout toolbarLayout);
 
     ToolbarLayout createToolbar() {
         return new ToolbarLayout(this)
                 .addTitle(getTitle().toString(), Color.WHITE, Color.BLACK, 16, Typeface.DEFAULT_BOLD)
-                .addBackIcon(R.drawable.ic_back_gray_ripple, 0, v -> {
-                });
+                .addBackIcon(com.zbase.R.drawable.ic_back_gray_ripple, 0, v -> finish());
     }
 
 }
