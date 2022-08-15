0585efb62d68ae1c3a5c2e78daeb10f4040fae2c
sharp <sharpchencn@gmail.com>
2022-08-15 16:29:30 +0800

    Move app used color to it's own module

--- src/main/java/com/zbase/x/ColorX.java
+++ src/main/java/com/zbase/x/ColorX.java
@@ -4,7 +4,7 @@ import android.graphics.Color;
 
 import androidx.annotation.ColorInt;
 
-public final class ColorX extends Color {
+public class ColorX extends Color {
     /*
      * Black
      */
@@ -32,14 +32,6 @@ public final class ColorX extends Color {
     /*
      * Blue
      */
-    @ColorInt
-    public static final int BLUE_3700B3 = 0xFF3700B3;
-
-    @ColorInt
-    public static final int BLUE_6200EE = 0xFF6200EE;
-
-    @ColorInt
-    public static final int BLUE_669EFC = 0xFF669EFC;
 
     @ColorInt
     public static final int BLUE_606D96 = 0xFF606D96;
@@ -49,24 +41,6 @@ public final class ColorX extends Color {
      */
     @ColorInt
     public static final int GREEN_549588 = 0xFF549588;
-
-    @ColorInt
-    public static final int GREEN_018786 = 0xFF018786;
-
-    @ColorInt
-    public static final int GREEN_03DAC5 = 0xFF03DAC5;
-
-    @ColorInt
-    public static final int GREEN_81C978 = 0xFF81C978;
-
-    /*
-     * Red
-     */
-    @ColorInt
-    public static final int RED_B87174 = 0xFFB87174;
-
-    @ColorInt
-    public static final int RED_BB86FC = 0xFFBB86FC;
 
     /**
      * Divider color
