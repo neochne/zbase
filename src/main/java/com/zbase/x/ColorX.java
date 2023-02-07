01bb907172024208fd6427927223d1892be3b2d6
sharp <sharpchencn@gmail.com>
2023-02-07 11:38:20 +0800

    feat: Add ListView divider default color

--- src/main/java/com/zbase/x/ColorX.java
+++ src/main/java/com/zbase/x/ColorX.java
@@ -20,6 +20,24 @@ public class ColorX extends Color {
     @ColorInt
     public static final int GRAY_353535 = 0xFF353535;
 
+    /**
+     * ListView 分隔线默认颜色（亮色主题时用，颜色较深一点）
+     */
+    @ColorInt
+    public static final int GRAY_E0E0E0 = 0xFFE0E0E0;
+
+    /**
+     * ListView 分隔线默认颜色（暗色主题时用，颜色较浅一点）
+     */
+    @ColorInt
+    public static final int GRAY_F3F3F3 = 0xFFF3F3F3;
+
+    /**
+     * 可做 Activity 背景颜色
+     */
+    @ColorInt
+    public static final int GRAY_EEEEEE = 0xFFEEEEEE;
+
     /*
      * Blue
      */
